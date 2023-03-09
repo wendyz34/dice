7217af4aaf29e5963f22c74299767ebd3cb0c4a1
BT_3W2_15 <wendyz34@nycstudents.net>
2023-03-09 09:27:57 -0500

    print

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[]args){
-    }
-        int[][] sample = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+
+       // int[][] sample = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
         runner player = new runner("player");
         System.out.println(player.fillGrid());
+        System.out.println("Score: " + player.calculate());
+
     }
 }
--- src/runner.java
+++ src/runner.java
@@ -1,36 +1,87 @@
-import java.util.Arrays;
+import java.util.*;
 import java.util.Scanner;
 public class runner {
-    private int[][]grid;
+    private int[][]grid=new int[2][2];
+    private ArrayList<Integer>num = new ArrayList<Integer>();
 
     public runner(String player){
     }
-    public void grid(){
-        grid = new int[5][5];
+    public int[][] grid(){
         for(int[] s: grid){
             System.out.println(Arrays.toString(s));
         }
-
+     return grid;
     }
     public int randNum(){
         int num = (int)(Math.random()*10)+1;
         return num;
     }
-    public int fillGrid(){
+    public int[][] fillGrid(){
         int num=0;
+        int c =0;
             Scanner scan = new Scanner(System.in);
-            System.out.println(grid);
-            num = randNum();
-            System.out.println("The num is: " + num);
-            System.out.println("Which row do you want it in?: ");
-            int row = scan.nextInt();
-            System.out.println("Which column?: ");
-            int column = scan.nextInt();
-            grid[row][column] = num;
+            while(c!=4) {
+                grid();
+                num = randNum();
+                System.out.println("The num is: " + num);
+                System.out.println("Which row do you want it in?: ");
+                int row = scan.nextInt();
+                System.out.println("Which column?: ");
+                int column = scan.nextInt();
+                grid[row][column] = num;
+                c++;
+            }
 
-        return num;
+        return grid();
     }
     public int calculate(){
+        //count row num
+        int sumRow =0;
+        int sumColumn=0;
+        int count =0;
+        for(int r =0;r< grid.length;r++){
+            for(int c=0;c<grid[0].length;c++){
+                num.add(grid[r][c]);
+               // System.out.println(Arrays.toString(num));
+               /* for(int s=0;s<num.size();s++){
+                    if(num.get(s)==num.get(s+1)) count++;
+                    if(count==1){
+                        num.remove(num.get(s));
+                        s--;
+                    }*/
+                for (int i = 0; i < num.size(); i++) {
+                    for (int j = 0; j <num.size(); j++) {
+                        if (num.get(i) == num.get(j)) sumRow+=num.get(i);
+
+                    }
+                   /* for(int x:num){
+                        sumRow+=x;
+                    }*/
+                    num.clear();
+                }
+
+            }
+        }
+        //count column sum
+       /* for(int c =0;c< grid[0].length;c++){
+            for(int r=0;r<grid.length;r++){
+                num.add(grid[r][c]);
+                System.out.println(num);
+                for(int s=0;s<num.size();s++){
+                    if(num.contains(num.get(s))) count++;
+                    if(count==1){
+                        num.remove(num.get(s));
+                        s--;
+                    }
+                    for(int x:num){
+                        sumRow+=x;
+                    }
+                }
+
+            }
+            num.clear();
+        }*/
+        return sumRow+sumColumn;
 
     }
 
